d4d9e949302de7a37e86d34a06e122826ee59fce
simon <simon.schoeggler@hotmail.com>
2023-02-27 12:15:37 +0100

    Initial commit

---
+++ src/main/java/at/kaindorf/LogLevel.java
@@ -0,0 +1,11 @@
+package at.kaindorf;
+/**
+* <h3>Created by IntelliJ IDEA.</h3><br>
+* <b>Project:</b> Logger<br>
+* <b>User:</b> Simon Schoeggler<br>
+* <b>Date:</b> 27. Februar 2023<br>
+* <b>Time:</b> 11:47<br>
+*/
+
+public enum LogLevel {
+}
